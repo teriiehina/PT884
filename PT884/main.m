51704bc9cec07119eb092579158845f079358768
Peter Meuel <pmeuel@mystudiofactory.com>
2013-06-24 15:11:07 +0200

    Initial Commit

---
+++ PT884/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PT884
+//
+//  Created by Peter MEUEL on 24/06/13.
+//  Copyright (c) 2013 Peter MEUEL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
